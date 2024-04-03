c70cad1d08afe469b1a7efc9f7e7664bdd521daa
SLASH-7 <basnetujjwal98@gmail.com>
2024-04-03 12:50:32 +0545

    error fixed

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -5,16 +5,15 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import Form from "@components/Form";
 const CreatePrompt = () => {
-  const [Post, setPost] = useState({
-    title: "",
+  const [post, setPost] = useState({
+    prompt: "",
     tag: "",
   });
 
   const createPromptHanlder = async (e) => {
     e.preventDefault();
-    
   };
-  return <Form Post={Post} createPromptHanlder={createPromptHanlder} />;
+  return <Form post={post} createPromptHanlder={createPromptHanlder} />;
 };
 
 export default CreatePrompt;
--- components/Form.jsx
+++ components/Form.jsx
@@ -1,6 +1,6 @@
 import { create } from "domain";
 
-const Form = ({ Post, createPromptHandler }) => {
+const Form = ({ post, createPromptHandler }) => {
   return (
     <>
       <section className="flex justify-center items-center h-screen w-full ">
@@ -11,15 +11,25 @@ const Form = ({ Post, createPromptHandler }) => {
           <label htmlFor="prompt" className="text-lg">
             Prompt
           </label>
-          <input
-            type="text"
-            value={Post.prompt}
-            onChange={() => setPost.prompt(e.target.value)}
-          />
+          <textarea
+            value={post.prompt}
+            onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+            placeholder="Write your prompt here..."
+            required
+            cols="30"
+            rows="10"
+          ></textarea>
           <label htmlFor="tag" className="text-lg">
             Tag
           </label>
-          <input type="text" onChange={Post.tag} />
+          <input
+            type="text"
+            required
+            value={post.tag}
+            onChange={(e) => setPost({ ...post, tag: e.target.value })}
+            placeholder="#tag"
+          />
+          
           <input type="submit" className="fourth-bg secondary-text" />
         </form>
       </section>
